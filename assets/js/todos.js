94ff2302f6236f1f4912bc3c10defa17c3e1b1bd
hellcat178 <xiaobohe178@gmail.com>
2020-08-07 00:48:04 -0400

    initial commit, fadeout added

---
+++ assets/js/todos.js
@@ -0,0 +1,12 @@
+//Check Off Specific Todos By Clicking
+$("li").click(function(){
+    $(this).toggleClass("completed");
+});
+
+//Click on X to delete Todo
+$("span").click(function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation;
+});
